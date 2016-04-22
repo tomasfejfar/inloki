3fbf2151e760359e3d8af49a4825b9616409c81f
Ignac Csosz <ignac.csosz@gmail.com>
2016-04-22 22:02:17 +0200

    Event filtering added Some throtteling added to event handling

--- app/src/main/java/com/hackaton/inloki/inloki/MainActivity.java
+++ app/src/main/java/com/hackaton/inloki/inloki/MainActivity.java
@@ -1,19 +1,20 @@
 package com.hackaton.inloki.inloki;
 
-import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import com.kontakt.sdk.android.ble.configuration.ActivityCheckConfiguration;
-import com.kontakt.sdk.android.ble.configuration.ForceScanConfiguration;
 import com.kontakt.sdk.android.ble.configuration.ScanPeriod;
-import com.kontakt.sdk.android.ble.configuration.scan.EddystoneScanContext;
 import com.kontakt.sdk.android.ble.configuration.scan.IBeaconScanContext;
 import com.kontakt.sdk.android.ble.configuration.scan.ScanContext;
 import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
 import com.kontakt.sdk.android.ble.discovery.BluetoothDeviceEvent;
+import com.kontakt.sdk.android.ble.discovery.EventType;
+import com.kontakt.sdk.android.ble.discovery.ibeacon.IBeaconAdvertisingPacket;
+import com.kontakt.sdk.android.ble.filter.ibeacon.IBeaconFilter;
 import com.kontakt.sdk.android.ble.manager.ProximityManager;
 import com.kontakt.sdk.android.ble.manager.ProximityManagerContract;
 import com.kontakt.sdk.android.common.KontaktSDK;
@@ -21,7 +22,11 @@ import com.kontakt.sdk.android.common.profile.DeviceProfile;
 import com.kontakt.sdk.android.common.profile.RemoteBluetoothDevice;
 import com.kontakt.sdk.android.manager.KontaktProximityManager;
 
+import java.util.Collections;
+import java.util.Date;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements ProximityManager.ProximityListener {
 
@@ -29,16 +34,23 @@ public class MainActivity extends AppCompatActivity implements ProximityManager.
 
     private ProximityManagerContract proximityManager;
     private ScanContext scanContext;
+    private TextView textView;
+    private long lastMessageTS;
+    private int lastProximity = 100;
+    private RelativeLayout contentLayout;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         KontaktSDK.initialize("api-key");
+        this.lastMessageTS = new Date().getTime();
         proximityManager = new KontaktProximityManager(this);
     }
 
     protected void onStart() {
         super.onStart();
+        this.textView = (TextView) findViewById(R.id.result_text);
+        this.contentLayout = (RelativeLayout) findViewById(R.id.content_layout);
         proximityManager.initializeScan(getScanContext(), new OnServiceReadyListener() {
             public void onServiceReady() {
                 proximityManager.attachListener(MainActivity.this);
@@ -60,23 +72,43 @@ public class MainActivity extends AppCompatActivity implements ProximityManager.
         if (scanContext == null) {
             scanContext = new ScanContext.Builder()
                     .setScanPeriod(ScanPeriod.RANGING) // or for monitoring for 15 seconds scan and 10 seconds waiting:
-                    //.setScanPeriod(new ScanPeriod(TimeUnit.SECONDS.toMillis(15), TimeUnit.SECONDS.toMillis(10)))
+//                    .setScanPeriod(new ScanPeriod(TimeUnit.SECONDS.toMillis(10), TimeUnit.SECONDS.toMillis(5)))
                     .setScanMode(ProximityManager.SCAN_MODE_BALANCED)
-                    .setActivityCheckConfiguration(ActivityCheckConfiguration.MINIMAL)
-                    .setForceScanConfiguration(ForceScanConfiguration.MINIMAL)
-                    .setIBeaconScanContext(new IBeaconScanContext.Builder().build())
-                    .setEddystoneScanContext(new EddystoneScanContext.Builder().build())
-                    .setForceScanConfiguration(ForceScanConfiguration.MINIMAL)
+                    .setIBeaconScanContext(new IBeaconScanContext.Builder()
+                        .setEventTypes(EnumSet.of(
+                                EventType.DEVICE_DISCOVERED,
+                                EventType.DEVICE_LOST,
+                                EventType.DEVICES_UPDATE
+                        ))
+                        .setIBeaconFilters(Collections.singleton(new IBeaconFilter() {
+                            public boolean apply(IBeaconAdvertisingPacket iBeaconAdvertisingPacket) {
+                                final String uniqId = iBeaconAdvertisingPacket.getBeaconUniqueId();
+
+                                return uniqId != null && uniqId.equals("5wKj");
+                            }
+                        }))
+                        .setDevicesUpdateCallbackInterval(TimeUnit.SECONDS.toMillis(1))
+                        .build()
+                    )
                     .build();
         }
         return scanContext;
     }
 
     public void onEvent(BluetoothDeviceEvent bluetoothDeviceEvent) {
+        long now = new Date().getTime();
+        long deltaTime = now - this.lastMessageTS;
+
+        if (deltaTime < 200) {
+            return;
+        }
+
+        boolean shouldShowMessage = true;
         List<? extends RemoteBluetoothDevice> deviceList = bluetoothDeviceEvent.getDeviceList();
         long timestamp = bluetoothDeviceEvent.getTimestamp();
         DeviceProfile deviceProfile = bluetoothDeviceEvent.getDeviceProfile();
         String text = "Unknown event!";
+        int proximity = this.lastProximity;
 
         switch (bluetoothDeviceEvent.getEventType()) {
             case SPACE_ENTERED:
@@ -95,12 +127,26 @@ public class MainActivity extends AppCompatActivity implements ProximityManager.
                 text = "namespace or region abandoned";
                 break;
         }
-        Log.d(TAG, text);
-        Context context = getApplicationContext();
-        int duration = Toast.LENGTH_SHORT;
+        if (!deviceList.isEmpty()) {
+            for (RemoteBluetoothDevice beacon : deviceList) {
+                text += "| " + beacon.getName()
+                        + "| " + beacon.getUniqueId()
+                        + "| " + beacon.getDistance()
+                        + "| " + beacon.getProximity().ordinal()
+                ;
+                proximity = this.getOwnProximity(beacon.getDistance());
+                break;
+            }
+        }
+        Log.d(TAG, text + " really?" + shouldShowMessage + " | " + proximity);
 
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+//        if (shouldShowMessage && this.lastProximity != proximity) {
+        if (shouldShowMessage) {
+            this.textView.setText(text);
+            this.contentLayout.setBackgroundColor(this.getDistanceColor(proximity));
+            this.lastMessageTS = now;
+            this.lastProximity = proximity;
+        }
     }
 
     public void onScanStart() {
@@ -109,5 +155,35 @@ public class MainActivity extends AppCompatActivity implements ProximityManager.
 
     public void onScanStop() {
         Log.d(TAG, "scan stopped");
+    }
+
+    private int getOwnProximity(double dist) {
+        if (dist < 0.6) {
+            return 0;
+//        } else if (dist < 0.6) {
+//            return 1;
+//        } else if (dist < 0.9) {
+//            return 2;
+//        } else if (dist < 1.5) {
+//            return 3;
+//        } else if (dist < 2) {
+//            return 4;
+        } else {
+            return 100;
+        }
+    }
+
+    private int getDistanceColor(int proximity) {
+        int color;
+        switch (proximity) {
+            case 0:
+                color = Color.rgb(0, 255, 0);
+                break;
+            default:
+                color = Color.rgb(255, 0, 0);
+                break;
+        }
+
+        return color;
     }
 }
