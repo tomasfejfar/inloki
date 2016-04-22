78881344ed45810b494a8d6505a6765add96b761
Vítězslav Šimek <vita.simek@gmail.com>
2016-04-22 20:15:13 +0100

    Respond only in json format for mac browser.

--- inloki.web/inloki.web/Global.asax.cs
+++ inloki.web/inloki.web/Global.asax.cs
@@ -18,6 +18,7 @@ namespace inloki.web
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
 		}
 	}
 }
