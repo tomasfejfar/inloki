cfcdeec300d46eef856c87181decdc2e02d97626
Vítězslav Šimek <vita.simek@gmail.com>
2016-04-22 15:44:17 +0100

    Initial import of template of the web admin application.

---
+++ inloki.web/inloki.web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace inloki.web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
