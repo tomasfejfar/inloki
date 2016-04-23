29d2d5176cde3c52dfa4ef2155ac6e4af6198de6
Vítězslav Šimek <vita.simek@gmail.com>
2016-04-23 08:22:06 +0100

    Missing conroller added, ignore packages content.

---
+++ inloki.web/inloki.web/Controllers/PathsController.cs
@@ -0,0 +1,19 @@
+﻿using inloki.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace inloki.web.Controllers
+{
+    public class PathsController : ApiController
+    {
+		// GET api/paths/55
+		public Path Get(string id)
+		{
+			return Database.Load().Paths.SingleOrDefault();
+		}
+	}
+}
